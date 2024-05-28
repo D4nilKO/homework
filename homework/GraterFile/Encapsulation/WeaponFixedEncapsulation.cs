18f495eb62f922c5484d4744bca019b2432cd2b3
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2024-05-28 14:33:27 +0300

    Add 1st attempt

---
+++ homework/GraterFile/Encapsulation/WeaponFixedEncapsulation.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace homework.GraterFile.Encapsulation.Fixed;
+
+class Weapon
+{
+    private int _damage;
+    private int _bullets;
+
+    public void Fire(Player player)
+    {
+        if (_bullets <= 0)
+        {
+            Debug.WriteLine("No more bullets, need to reload!");
+            return;
+        }
+
+        if (player.TryApplyDamage(_damage))
+        {
+            _bullets -= 1;
+        }
+    }
+}
+
+class Player
+{
+    private int _health;
+    
+    public bool TryApplyDamage(int damage)
+    {
+        if (damage < 0)
+        {
+            Debug.WriteLine("Damage can't be negative");
+            return false;
+        }
+
+        _health -= damage;
+        
+        if (_health <= 0)
+        {
+            Die();
+        }
+
+        return true;
+    }
+
+    private void Die()
+    {
+        // do something
+    }
+}
+
+class Bot
+{
+    private Weapon _weapon;
+
+    public void OnSeePlayer(Player player)
+    {
+        _weapon.Fire(player);
+    }
+}
