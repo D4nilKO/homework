ca1e4acc1d4c4fb5080ea86af411bdf171ef7427
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2024-05-29 19:51:16 +0300

    2 attempt

--- homework/GraterFile/Encapsulation/WeaponFixedEncapsulation.cs
+++ homework/GraterFile/Encapsulation/WeaponFixedEncapsulation.cs
@@ -1,14 +1,34 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace homework.GraterFile.Encapsulation.Fixed;
 
 class Weapon
 {
+    public Weapon(int damage, int bullets)
+    {
+        if (damage >= 0)
+            _damage = damage;
+        else
+            throw new ArgumentException("Damage can't be negative");
+
+        if (_bullets >= 0)
+            _bullets = bullets;
+        else
+            throw new ArgumentException("Bullets can't be negative");
+    }
+
     private int _damage;
     private int _bullets;
 
     public void Fire(Player player)
     {
+        if (player == null)
+        {
+            Debug.WriteLine("Player can't be null");
+            return;
+        }
+
         if (_bullets <= 0)
         {
             Debug.WriteLine("No more bullets, need to reload!");
@@ -25,7 +45,33 @@ class Weapon
 class Player
 {
     private int _health;
-    
+
+    public Player(int health, int maxHealth)
+    {
+        if (health > 0)
+            Health = health;
+        else
+            throw new ArgumentException("Health can't be negative");
+
+        if (maxHealth > 0)
+            MaxHealth = maxHealth;
+        else
+            throw new ArgumentException("MaxHealth can't be negative");
+    }
+
+    private int MaxHealth { get; }
+
+    public int Health
+    {
+        get => _health;
+        private set => _health = UserUtils.Clamp(value, 0, MaxHealth);
+    }
+
+    private void Die()
+    {
+        // do something
+    }
+
     public bool TryApplyDamage(int damage)
     {
         if (damage < 0)
@@ -35,7 +81,7 @@ class Player
         }
 
         _health -= damage;
-        
+
         if (_health <= 0)
         {
             Die();
@@ -43,19 +89,40 @@ class Player
 
         return true;
     }
-
-    private void Die()
-    {
-        // do something
-    }
 }
 
 class Bot
 {
     private Weapon _weapon;
 
+    public Bot(Weapon weapon)
+    {
+        if (weapon != null)
+        {
+            _weapon = weapon;
+        }
+        else
+        {
+            throw new ArgumentException("Weapon can't be null");
+        }
+    }
+
     public void OnSeePlayer(Player player)
     {
         _weapon.Fire(player);
+    }
+}
+
+internal static class UserUtils
+{
+    public static int Clamp(int value, int min, int max)
+    {
+        if (value < min)
+            value = min;
+
+        if (value > max)
+            value = max;
+
+        return value;
     }
 }
